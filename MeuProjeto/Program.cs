fe6663f339a1794e46b7c72c994b7f4f85acc620
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-05-15 22:20:38 -0300

    Meu primeiro código em C#

---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Hello
+{
+    static void Main()
+    {
+        string nome = "Daniel";
+        // Estou gerando um Hello World
+        Console.WriteLine("Meu Hello, World! " + nome);
+    }
+}
