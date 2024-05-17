a73108cdfac87e22b7f89b8afe481f68ba99a4b1
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-05-17 20:51:13 -0300

    Estudando mais sobre Variáveis

---
+++ Dia 02/Conteudo/Conteudo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Conteudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomeUsuario = "Daniel";
+            int idade = 23;
+            string genero = "Masculino";
+            float altura = 1.75f; // O número é decimal? use o F no final
+            bool direitos = true;
+
+            
+            Console.WriteLine(nomeUsuario);
+            Console.WriteLine(idade);
+            Console.WriteLine(genero);
+            Console.WriteLine(altura + " de altura");
+            Console.WriteLine(direitos);
+        }
+    }
+}
