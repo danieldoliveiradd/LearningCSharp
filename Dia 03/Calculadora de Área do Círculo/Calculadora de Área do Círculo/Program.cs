d2396db777cfd06ab50a9529fb1756ed331de150
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-05-18 22:55:45 -0300

    Calculadora de Área do Círculo - Dia 03

---
+++ Dia 03/Calculadora de Área do Círculo/Calculadora de Área do Círculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, insira o valor do raio do círculo: ");
+            double raioCirculo = double.Parse(Console.ReadLine());
+
+            double pi = Math.PI;
+
+            double circunferencia = 2 * pi * raioCirculo;
+            Console.WriteLine("A circunferência do círculo é: " + circunferencia);
+
+            double area = pi * Math.Pow(raioCirculo, 2);
+            Console.WriteLine("A área do círculo é: " + area);
+        }
+    }
+}
---
+++ Dia 03/Condicionais/Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a sua nota em Matemática ");
+            int notaMatematica = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a sua nota em Português: ");
+            int notaPortugues = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a sua nota em Geografia: ");
+            int notaGeografia = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a sua nota em Informática: ");
+            int notaInformatica = int.Parse(Console.ReadLine());
+
+            int media = (notaMatematica + notaPortugues + notaGeografia + notaInformatica) / 4;
+
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Você passou parabéns! a sua média é: " + media);
+            }
+            else
+            {
+                Console.WriteLine("Você reprovou! a sua média é: " + media);
+            }
+
+        }
+    }
+}
