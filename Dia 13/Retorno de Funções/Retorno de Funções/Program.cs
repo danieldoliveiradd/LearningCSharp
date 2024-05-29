1be663d74577382cf3bb22f1f2cf159c45c6be91
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-05-28 22:58:25 -0300

    Aprofundando nos Arrays e Retornos de Funções - Dia 13

---
+++ Dia 13/Retorno de Funções/Retorno de Funções/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EstudandoFuncoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiraSoma = RetornandoSomas(2, 4, 6);
+            int segundaSoma = RetornandoSomas(3, 6, 9);
+            int terceiraSoma = RetornandoSomas(4, 8, 12);
+            Console.WriteLine($"Os resultados são: {primeiraSoma} + {segundaSoma} + {terceiraSoma}");
+
+            Console.WriteLine("Um array de carros:");
+            TrabalhandoComArray();
+        }
+
+        static int RetornandoSomas(int x, int y, int z)
+        {
+            int soma = x + y + z;
+            return soma;
+        }
+
+        static void TrabalhandoComArray()
+        {
+            string[] carros = new string[5] 
+            { 
+                "Fiat Argo", 
+                "Fiat Pulse", 
+                "Fiat Fastback", 
+                "Fiat Punto", 
+                "Fiat Uno" 
+            };
+
+            Console.WriteLine(carros[1]);
+
+        }
+    }
+}
