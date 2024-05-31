b9346bf3a0cd56faaf02954559dbfe0ebfe401d1
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-05-31 02:36:07 -0300

    Finalizando Curso C# - Dia 15

---
+++ Dia 15/Curso C#/Curso C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FinalizandoCurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor = Console.ReadLine();
+
+            switch (cor)
+            {
+                case "Vermelho": 
+                        Console.WriteLine("Sua cor favorita é: vermelho");
+                    break;
+                case "Amarelo":
+                    Console.WriteLine("Sua cor favorita é: amarelo");
+                    break;
+                default:
+                    Console.WriteLine("Esta cor não existe");
+                    break;
+            }
+        }
+    }
+}
